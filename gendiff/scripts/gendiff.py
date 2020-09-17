d3b17250117cb205a9ed7b1a3e4328fcb41a63d7
agmrv <airatgum@gmail.com>
2020-09-17 20:33:50 +0500

    project init

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,11 @@
+"""Script of 'Difference Calculator'."""
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    main()

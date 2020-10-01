274589d54ed99b8fcf36a6ffe656648273ba0a53
agmrv <airatgum@gmail.com>
2020-10-01 19:36:06 +0500

    add value conver for tree

--- gendiff/upload_file.py
+++ gendiff/upload_file.py
@@ -19,6 +19,20 @@ MAPPING_FOR_CONVERT_VALUE = types.MappingProxyType({
 })
 
 
+def convert_values(some_item, extension):
+    """Convert dict values to initial format.
+
+    Args:
+        some_item: dict to convert
+        extension: file extension
+    """
+    for key, some_value in some_item.items():
+        if isinstance(some_value, dict):
+            convert_values(some_value, extension)
+        elif not isinstance(some_value, str):
+            some_item[key] = MAPPING_FOR_CONVERT_VALUE[extension](some_value)
+
+
 def get_file(filepath):
     """Upload file from filepath.
 
@@ -31,8 +45,5 @@ def get_file(filepath):
     filename = basename(filepath)
     _, extension = filename.split('.')
     file_object = MAPPING_FOR_UPLOAD[extension](filepath)
-    for key, some_value in file_object.items():
-        if not isinstance(some_value, str):
-            # convert data to initial format
-            file_object[key] = MAPPING_FOR_CONVERT_VALUE[extension](some_value)
+    convert_values(file_object, extension)  # convert data to initial format
     return file_object

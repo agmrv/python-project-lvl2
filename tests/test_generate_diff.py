f9966491cd9c06a14b823ed506e9ac878ab06989
agmrv <airatgum@gmail.com>
2020-10-01 19:34:06 +0500

    add test for tree JSON

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -4,7 +4,6 @@
 
 from gendiff import generate_diff
 from os.path import abspath
-import json
 
 
 def test_generate_diff_JSON():
@@ -22,6 +21,16 @@ def test_generate_diff_YAML():
     file1_abspath = abspath('tests/fixtures/before.yml')
     file2_abspath = abspath('tests/fixtures/after.yml')
     expected_abspath = abspath('tests/fixtures/compare_YAML.txt')
+    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
+        expected = compare.read()
+    assert (generate_diff(file1_abspath, file2_abspath)) == expected
+
+
+def test_generate_diff_tree_JSON():
+    """Test for tree JSON files."""
+    file1_abspath = abspath('tests/fixtures/before_tree.json')
+    file2_abspath = abspath('tests/fixtures/after_tree.json')
+    expected_abspath = abspath('tests/fixtures/compare_tree_JSON.txt')
     with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
         expected = compare.read()
     assert (generate_diff(file1_abspath, file2_abspath)) == expected

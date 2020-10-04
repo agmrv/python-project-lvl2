21718c8170f4a7adcbaf9287b7809a34c2b62e7e
agmrv <airatgum@gmail.com>
2020-10-04 14:06:34 +0500

    added helper functions for correct output

--- gendiff/upload_file.py
+++ gendiff/upload_file.py
@@ -14,10 +14,26 @@ MAPPING_FOR_UPLOAD = types.MappingProxyType({
     'yaml': lambda path: yaml.safe_load(open(path)),
 })
 
+
+def yaml_convert_value(element):
+    """Convert element to initial YAML format.
+
+    Args:
+        element: value to convert
+    
+    Returns:
+        string
+    """
+    if isinstance(element, str):
+        new_element = yaml.safe_dump(element).split('\n')[0]
+        return f'"{new_element}"'
+    return yaml.safe_dump(element).split('\n')[0]
+
+
 MAPPING_FOR_CONVERT_VALUE = types.MappingProxyType({
     'json': lambda element: json.dumps(element),
-    'yml': lambda element: yaml.safe_dump(element).split('\n')[0],
-    'yaml': lambda element: yaml.safe_dump(element).split('\n')[0],
+    'yml': yaml_convert_value,
+    'yaml': yaml_convert_value,
 })
 
 
@@ -31,7 +47,7 @@ def convert_values(some_item, extension):
     for key, some_value in some_item.items():
         if isinstance(some_value, dict):
             convert_values(some_value, extension)
-        elif not isinstance(some_value, str):
+        else:
             some_item[key] = MAPPING_FOR_CONVERT_VALUE[extension](some_value)
 
 
@@ -49,3 +65,31 @@ def get_file(filepath):
     file_object = MAPPING_FOR_UPLOAD[extension](filepath)
     convert_values(file_object, extension)  # convert data to initial format
     return file_object
+
+
+def was_string(element):
+    """Checks if it was a string before converting.
+    Needed for correct output format with or without quotes
+    in formatter functions.
+
+    Args:
+        element: value for check
+
+    Returns:
+        bool
+    """
+    return element.startswith('"') and element.endswith('"')
+
+
+def remove_dubleqoutes(element):
+    """Remove duble quotes from string.
+    Needed for correct output format with or without quotes
+    in formatter functions.
+
+    Args:
+        element: string for change
+
+    Returns:
+        string
+    """
+    return element[1:-1]

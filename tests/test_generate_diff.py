cc93ea1f9431d21b9987e83f39148eee0035d76b
agmrv <airatgum@gmail.com>
2020-10-17 20:53:17 +0500

    parametrizing test functions

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -3,124 +3,65 @@
 """'generate_diff' function tests."""
 
 from gendiff import generate_diff
-from os.path import abspath
+import pytest
 
 
-def test_generate_diff_JSON_json_like():
-    """Test for flat JSON files."""
-    file1_abspath = abspath('tests/fixtures/before.json')
-    file2_abspath = abspath('tests/fixtures/after.json')
-    expected_abspath = abspath('tests/fixtures/comparisons/compare_JSON.txt')
-    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
-        expected = compare.read()
-    assert (generate_diff(file1_abspath, file2_abspath, output_format='json-like')) == expected
+# flat files
+before_json = 'tests/fixtures/before.json'
+after_json = 'tests/fixtures/after.json'
+before_yaml = 'tests/fixtures/before.yml'
+after_yaml = 'tests/fixtures/after.yml'
+
+# tree files
+before_tree_json = 'tests/fixtures/before_tree.json'
+after_tree_json = 'tests/fixtures/after_tree.json'
+before_tree_yaml = 'tests/fixtures/before_tree.yml'
+after_tree_yaml = 'tests/fixtures/after_tree.yml'
+
+# comparisons
+json = 'tests/fixtures/comparisons/compare_json.txt'
+plain = 'tests/fixtures/comparisons/compare_plain.txt'
+string = 'tests/fixtures/comparisons/compare_string.txt'
+tree_json = 'tests/fixtures/comparisons/compare_tree_json.txt'
+tree_plain = 'tests/fixtures/comparisons/compare_tree_plain.txt'
+tree_string = 'tests/fixtures/comparisons/compare_tree.txt'
+
+FLAT_FILES = [
+    (before_json, after_json),
+    (before_yaml, after_yaml),
+]
+
+FLAT_COMPARISONS = [
+    (json, 'json'),
+    (plain, 'plain'),
+    (string, 'json-like'),
+]
+
+TREE_FILES = [
+    (before_tree_json, after_tree_json),
+    (before_tree_yaml, after_tree_yaml),
+]
+
+TREE_COMPARISONS = [
+    (tree_json, 'json'),
+    (tree_plain, 'plain'),
+    (tree_string, 'json-like'),
+]
 
 
-def test_generate_diff_YAML_json_like():
-    """Test for flat YAML files."""
-    file1_abspath = abspath('tests/fixtures/before.yml')
-    file2_abspath = abspath('tests/fixtures/after.yml')
-    expected_abspath = abspath('tests/fixtures/comparisons/compare_YAML.txt')
-    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
-        expected = compare.read()
-    assert (generate_diff(file1_abspath, file2_abspath)) == expected
+@pytest.mark.parametrize('result, output_format', FLAT_COMPARISONS, ids=['json', 'plain', 'string'])
+@pytest.mark.parametrize('before, after', FLAT_FILES, ids=['json', 'yaml'])
+def test_generate_diff_for_flat_files(before, after, result, output_format):
+    with open(result, mode='r', encoding='UTF-8') as r:
+        expected = r.read()
+        output = generate_diff(before, after, output_format)
+        assert output == expected
 
 
-def test_generate_diff_tree_JSON_json_like():
-    """Test for tree JSON files."""
-    file1_abspath = abspath('tests/fixtures/before_tree.json')
-    file2_abspath = abspath('tests/fixtures/after_tree.json')
-    expected_abspath = abspath('tests/fixtures/comparisons/compare_tree_JSON.txt')
-    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
-        expected = compare.read()
-    assert (generate_diff(file1_abspath, file2_abspath)) == expected
-
-
-def test_generate_diff_tree_YAML_json_like():
-    """Test for tree JSON files."""
-    file1_abspath = abspath('tests/fixtures/before_tree.yml')
-    file2_abspath = abspath('tests/fixtures/after_tree.yml')
-    expected_abspath = abspath('tests/fixtures/comparisons/compare_tree_YAML.txt')
-    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
-        expected = compare.read()
-    assert (generate_diff(file1_abspath, file2_abspath)) == expected
-
-
-def test_generate_diff_JSON_plain():
-    """Test for flat JSON files."""
-    file1_abspath = abspath('tests/fixtures/before.json')
-    file2_abspath = abspath('tests/fixtures/after.json')
-    expected_abspath = abspath('tests/fixtures/comparisons/compare_JSON_plain.txt')
-    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
-        expected = compare.read()
-    assert (generate_diff(file1_abspath, file2_abspath, output_format='plain')) == expected
-
-
-def test_generate_diff_YAML_plain():
-    """Test for flat YAML files."""
-    file1_abspath = abspath('tests/fixtures/before.yml')
-    file2_abspath = abspath('tests/fixtures/after.yml')
-    expected_abspath = abspath('tests/fixtures/comparisons/compare_YAML_plain.txt')
-    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
-        expected = compare.read()
-    assert (generate_diff(file1_abspath, file2_abspath, output_format='plain')) == expected
-
-
-def test_generate_diff_tree_JSON_plain():
-    """Test for tree JSON files."""
-    file1_abspath = abspath('tests/fixtures/before_tree.json')
-    file2_abspath = abspath('tests/fixtures/after_tree.json')
-    expected_abspath = abspath('tests/fixtures/comparisons/compare_tree_JSON_plain.txt')
-    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
-        expected = compare.read()
-    assert (generate_diff(file1_abspath, file2_abspath, output_format='plain')) == expected
-
-
-def test_generate_diff__tree_YAML_plain():
-    """Test for tree YAML files."""
-    file1_abspath = abspath('tests/fixtures/before_tree.yml')
-    file2_abspath = abspath('tests/fixtures/after_tree.yml')
-    expected_abspath = abspath('tests/fixtures/comparisons/compare_tree_YAML_plain.txt')
-    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
-        expected = compare.read()
-    assert (generate_diff(file1_abspath, file2_abspath, output_format='plain')) == expected
-
-
-def test_generate_diff_JSON_json():
-    """Test for flat JSON files."""
-    file1_abspath = abspath('tests/fixtures/before.json')
-    file2_abspath = abspath('tests/fixtures/after.json')
-    expected_abspath = abspath('tests/fixtures/comparisons/compare_JSON_json.txt')
-    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
-        expected = compare.read()
-    assert (generate_diff(file1_abspath, file2_abspath, output_format='json')) == expected
-
-
-def test_generate_diff_YAML_json():
-    """Test for flat YAML files."""
-    file1_abspath = abspath('tests/fixtures/before.yml')
-    file2_abspath = abspath('tests/fixtures/after.yml')
-    expected_abspath = abspath('tests/fixtures/comparisons/compare_YAML_json.txt')
-    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
-        expected = compare.read()
-    assert (generate_diff(file1_abspath, file2_abspath, output_format='json')) == expected
-
-
-def test_generate_diff_tree_JSON_json():
-    """Test for tree JSON files."""
-    file1_abspath = abspath('tests/fixtures/before_tree.json')
-    file2_abspath = abspath('tests/fixtures/after_tree.json')
-    expected_abspath = abspath('tests/fixtures/comparisons/compare_tree_JSON_json.txt')
-    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
-        expected = compare.read()
-    assert (generate_diff(file1_abspath, file2_abspath, output_format='json')) == expected
-
-
-def test_generate_diff__tree_YAML_json():
-    """Test for tree YAML files."""
-    file1_abspath = abspath('tests/fixtures/before_tree.yml')
-    file2_abspath = abspath('tests/fixtures/after_tree.yml')
-    expected_abspath = abspath('tests/fixtures/comparisons/compare_tree_YAML_json.txt')
-    with open(expected_abspath, mode='r', encoding='UTF-8') as compare:
-        expected = compare.read()
-    assert (generate_diff(file1_abspath, file2_abspath, output_format='json')) == expected
+@pytest.mark.parametrize('result, output_format', TREE_COMPARISONS, ids=['json', 'plain', 'string'])
+@pytest.mark.parametrize('before, after', TREE_FILES, ids=['tree_json', 'tree_yaml'])
+def test_generate_diff_for_tree_files(before, after, result, output_format):
+    with open(result, mode='r', encoding='UTF-8') as r:
+        expected = r.read()
+        output = generate_diff(before, after, output_format)
+        assert output == expected

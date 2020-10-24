a147a6f48ff359b5d8e8ac96700b12bacb9265d5
agmrv <airatgum@gmail.com>
2020-10-24 12:48:13 +0500

    parametrized test

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -5,43 +5,49 @@
 from os import getcwd
 from os.path import abspath
 
+import pytest
+
 from gendiff import generate_diff
 
 
-def test_invalid_output_format():
-    """Test for 'Invalid Output Format' case."""
-    file1 = 'tests/fixtures/before.json'
-    file2 = 'tests/fixtures/after.json'
-    expected = (
-        "Invalid output format: 'invalid-format'.\n"
-        +
-        "Try 'json', 'plain' or 'stylish'."
-    )
-    assert generate_diff(file1, file2, output_format='invalid-format') == expected
+@pytest.mark.parametrize(
+    'before, after, output_format, expected',
+    [
+        (
+            'tests/fixtures/before.json',
+            'tests/fixtures/after.json',
+            'invalid-format',
+            "Invalid output format: 'invalid-format'.\n"
+            + "Try 'json', 'plain' or 'stylish'.",
+        ),
+        (
+            'tests/fixtures/before.xml',
+            'tests/fixtures/after.json',
+            'stylish',
+            "Unsupported file extension: '.xml'\n"
+            + "'.json' and '.yaml' are supported.",
+        ),
+        (
+            abspath('tests/fixtures/nonexistent.json'),
+            abspath('tests/fixtures/after.json'),
+            'stylish',
+            'File not found.\nNo such file or directory: '
+            + "'{0}/tests/fixtures/nonexistent.json'".format(getcwd()),
+        ),
+    ],
+    ids=[
+        'Invalid Output Format',
+        'Unsuppported File Extension',
+        'File Not Found',
+    ],
+)
+def test_exception(before, after, output_format, expected):
+    """Test exception.
 
-
-def test_unsupported_file_extension():
-    """Test for 'Unsuppported File Extension' case."""
-    file1 = 'tests/fixtures/before.xml'
-    file2 = 'tests/fixtures/after.json'
-    expected = (
-        "Unsupported file extension: '.xml'\n"
-        +
-        "'.json' and '.yaml' are supported."
-    )
-    assert generate_diff(file1, file2) == expected
-
-
-def test_file_not_found():
-    """Test for 'File Not Found' case."""
-    file1_abspath = abspath('tests/fixtures/nonexistent.json')
-    file2_abspath = abspath('tests/fixtures/after.json')
-    cwd = getcwd()
-    expected = (
-        'File not found.\nNo such file or directory: '
-        +
-        "'{0}/tests/fixtures/nonexistent.json'".format(cwd)
-    )
-    assert generate_diff(file1_abspath, file2_abspath) == expected
-
-
+    Args:
+        before: before filepath
+        after: after filepath
+        output_format: output format
+        expected: expected string
+    """
+    assert generate_diff(before, after, output_format) == expected
